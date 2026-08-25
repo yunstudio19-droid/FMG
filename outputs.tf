30dc25807e33ee0f1ac2dd7e410a1d147b9d132d
yunstudio19-droid <yunstudio19@gmail.com>
2026-08-25 13:21:46 +0800

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,11 @@ output "subnet_id" {
   value       = aws_subnet.public.id
 }
 
+output "security_group_id" {
+  description = "Security group ID"
+  value       = aws_security_group.server.id
+}
+
 output "instance_id" {
   description = "EC2 instance ID"
   value       = aws_instance.server.id
@@ -18,7 +23,12 @@ output "public_ip" {
   value       = aws_instance.server.public_ip
 }
 
+output "public_dns" {
+  description = "EC2 public DNS"
+  value       = aws_instance.server.public_dns
+}
+
 output "website_url" {
-  description = "Web server URL"
+  description = "Website URL"
   value       = "http://${aws_instance.server.public_ip}"
 }
